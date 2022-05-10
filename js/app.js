0de91d76c424bc78f979c8f1d7b3a0e1ce325ca4
MicroScender <microcosmic_transcender@protonmail.com>
2022-05-10 12:28:04 -0700

    pushing data Audrey provided

---
+++ js/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+console.log('hey world hey'); 
+
+let meepers1 =prompt('Do I have a diaper?').toLowerCase();
+
+if(meepers1 ==='yes'|| meepers1==='y'){
+alert('good deal. don\'t wanna soil yourself');
+}else{ ('Might wanna grab one so you can change yourself when you soil your diapee!');
+}
